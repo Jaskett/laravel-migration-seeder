fec22db885330988eda2c126d1e047de89455daa
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-14 15:50:52 +0100

    Creazione dati struttura dati

--- database/migrations/2023_03_14_143717_create_trains_table.php
+++ database/migrations/2023_03_14_143717_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time', 3);
+            $table->dateTime('arrival_time', 1);
+            $table->string('train_code', 5);
+            $table->tinyInteger('carriage_number')->unsigned();
+            $table->boolean('on_time')->default(0);
+            $table->boolean('is_canceled')->default(0);
             $table->timestamps();
         });
     }
